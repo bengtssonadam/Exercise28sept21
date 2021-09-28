e8b9ea06393b38eba2c739b0982c25bf070a5ac1
Joakim Wassberg <joakim@arthead.se>
2021-09-28 14:40:26 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1. Läs igenom hela boken, alice.txt och berätta hur många rader den innehåller
+        // 2. Gör samma sak, men räkna bara rader som innehåller text
+        // 3. Läs hela boken och räkna hur många ord som den består av
+        // 4. Läs igenom hela boken och kontrollera om alla bokstäver i det engelska alfabetet används
+        // 5. Läs igenom hela boken och räkna efter hur många gånger ordet the används. Tänk på att ett ord kan skrivas med både gemener och versaler. Ordet kan även vara sammanskrivet med ett skiljetecken, alltså the, eller the.
+        // 6. Räkna hur många gånger varje ord förekommer, tänk även här på gemener och versaler, så exempelvis All, all och ALL räknas som samma ord. Även här måste du tänka på skiljetecken. Skriv ut en lista med de 100 vanligaste orden och hur många gånger de förekommer
+        // 7. Skriv ut bokens längsta ord.
+    }
+}
